c47b2e40d4da33a44d62b7354abc12ba4c244fdf
Akshay Dhumal <akshay.dhumal@nimapinfotech.com>
2025-04-04 10:13:15 +0530

    6. WAP to find the given string is Palindrome or not.

--- Program.cs
+++ Program.cs
@@ -71,6 +71,64 @@
 //}
 
 // ]3. Logic for anagram program with its time complexity. (for large strings).
+//using System;
+//namespace logicals
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            string str1 = "listen";
+//            string str2 = "silemt";
+//            if (isAnagram(str1, str2))
+//            {
+//                Console.WriteLine("Anagram");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Not Anagram");
+//            }
+//        }
+
+//        public static bool isAnagram(string str1, string str2)
+//        {
+//            int[] array = new int[256];
+//            for (int i = 0; i < str1.Length; i++)
+//            {
+//                array[str1[i]]++;
+//                array[str2[i]]--;
+//            }
+//            foreach (var i in array)
+//            {
+//                if (i != 0)
+//                {
+//                    return false;
+//                }
+//            }
+//            return true;
+//        }
+//    }
+//}
+
+// 4. Find the reverse of the string.
+//using System;
+//using System.Linq;
+//namespace logicals
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            string str = "Akshay";
+//            var reversed = str.Reverse().ToArray();
+//            reversed.ToString();
+//            Console.WriteLine(reversed);
+//        }
+//    }
+//}
+
+
+// 6. WAP to find the given string is Palindrome or not.
 using System;
 namespace logicals
 {
@@ -78,32 +136,29 @@ namespace logicals
     {
         public static void Main(string[] args)
         {
-            string str1 = "listen";
-            string str2 = "silemt";
-            if (isAnagram(str1, str2))
+            string str = "abbna";
+            if (isPalindrome(str))
             {
-                Console.WriteLine("Anagram");
+                Console.WriteLine("Palindrome");
             }
             else
             {
-                Console.WriteLine("Not Anagram");
+                Console.WriteLine("Not Palindrome");
             }
         }
 
-        public static bool isAnagram(string str1, string str2)
+        public static bool isPalindrome(string str)
         {
-            int[] array = new int[256];
-            for (int i = 0; i < str1.Length; i++)
+            int left = 0;
+            int right = str.Length - 1;
+            while (left < right)
             {
-                array[str1[i]]++;
-                array[str2[i]]--;
-            }
-            foreach (var i in array)
-            {
-                if (i != 0)
+                if (str[left] != str[right])
                 {
                     return false;
                 }
+                left++;
+                right--;
             }
             return true;
         }
